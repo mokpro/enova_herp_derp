001ca27e35541cf3b50a7d73c024b5ddc740f74c
Pranav Moktali <pmoktali@enova.com>
2016-10-26 12:26:38 -0500

    uses light resistor to switch songs

--- src/player.cpp
+++ src/player.cpp
@@ -5,38 +5,43 @@
 // Serial MP3 Player Catalex (YX5300 chip)
 #include "mp3serial.h"
 int lightPin = 0;  //define a pin for Photo resistor
+bool low = false;
 
-
-void setup()
-{
+void setup() {
 	Serial.begin(9600);
 	mp3.begin(9600);
 	delay(500);
 
   sendCommand(CMD_SEL_DEV, DEV_TF);
 	delay(200);
-	sendCommand(CMD_PLAY_W_VOL, 0X0F02); // Playing a 15 (0x0F) Vol the song num 02 (0x02).
+	sendCommand(CMD_PLAY_W_VOL, 0X0F01); // Playing a 15 (0x0F) Vol the song num 01 (0x01).
 }
 
 
-void loop()
-{
- char c=' ';
+void loop() {
+ 	char c=' ';
+ 	int lightPinOutput;
 
- 	Serial.println(analogRead(lightPin));
-	delay(200); //short delay for faster response to light.
+ 	lightPinOutput = analogRead(lightPin);
+ 	delay(200);
 
-  // If there a char on Serial call sendMP3Command to sendCommand
-   if( Serial.available() )
-    {
-    c = Serial.read();
-    sendMP3Command(c);
-    }
+	if(lightPinOutput < 700 && !low) {
+		low = true;
+		c = 'n';
+	} else if(lightPinOutput >= 700){
+		low = false;
+	}
+
+
+  if( Serial.available() )
+		c = Serial.read();
+
+	if(c != ' ')
+		sendMP3Command(c);
 
   // Check for the answer.
   if (mp3.available())
-  {
     Serial.println(decodeMP3Answer());
-  }
+
   delay(100);
 }
