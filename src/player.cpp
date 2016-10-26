2f9b840b4cec008a8402cdb79cfb8501efd48938
Pranav Moktali <pmoktali@enova.com>
2016-10-26 13:32:23 -0500

    takes 2 inputs

--- src/player.cpp
+++ src/player.cpp
@@ -4,8 +4,12 @@
 
 // Serial MP3 Player Catalex (YX5300 chip)
 #include "mp3serial.h"
-int lightPin = 0;  //define a pin for Photo resistor
+int lightPin0 = 0;  //define a pin for Photo resistor
+int lightPin1 = 1;  //define a pin for Photo resistor
+
 bool low = false;
+int16_t lastSongPlayed = 0X0F00;
+
 
 void setup() {
 	Serial.begin(9600);
@@ -17,18 +21,44 @@ void setup() {
 	sendCommand(CMD_PLAY_W_VOL, 0X0F01); // Playing a 15 (0x0F) Vol the song num 01 (0x01).
 }
 
+void playSong(int16_t songToPlay) {
+
+	// Playing a 15 (0x0F) Vol the song num 01 (0x01).
+	if(lastSongPlayed != songToPlay) {
+		lastSongPlayed = songToPlay;
+		sendCommand(CMD_PLAY_W_VOL, songToPlay);
+	}
+}
 
 void loop() {
  	char c=' ';
- 	int lightPinOutput;
+ 	int lightPin0Reading;
+	int lightPin1Reading;
 
- 	lightPinOutput = analogRead(lightPin);
+ 	lightPin0Reading = analogRead(lightPin0);
+	lightPin1Reading = analogRead(lightPin1);
+
  	delay(200);
 
-	if(lightPinOutput < 700 && !low) {
+	// Serial.print("lightPin0Reading: ");
+	// Serial.print(lightPin0Reading);
+	// Serial.println("");
+	//
+	// Serial.print("lightPin1Reading: ");
+	// Serial.print(lightPin1Reading);
+	// Serial.println("");
+
+	if(lightPin0Reading < 700 && !low) {
 		low = true;
-		c = 'n';
-	} else if(lightPinOutput >= 700){
+		playSong(0X0F02);
+	} else if(lightPin0Reading >= 700){
+		low = false;
+	}
+
+	if(lightPin1Reading < 700 && !low) {
+		low = true;
+		playSong(0X0F04);
+	} else if(lightPin1Reading >= 700){
 		low = false;
 	}
 
